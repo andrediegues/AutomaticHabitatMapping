3159fdae4e42d24c5033fc0d390dbf89c28036b2
andrediegues <andrediegues@github.com>
2018-01-31 18:51:13 +0000

    preparing imc tranformation and adding a new result as an image

---
+++ imc.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 31 10:04:46 2018
+
+@author: diegues
+"""
+from matplotlib import pyplot as plt
+import imageFilters as imf
+import numpy as np
+import sys
+import cv2
+import itertools as it
+
+def linearContrastStretchingProcessing(img):
+    hashmap = {}
+    bot, top = min(img.ravel()), max(img.ravel())
+    rmin = np.percentile(img.ravel(), 1)
+    rmax = np.percentile(img.ravel(), 99)
+        
+    print(bot, top, rmin, rmax)
+    for i,j in it.product(range(img.shape[0]), range(img.shape[1])):   
+        if img[i,j] not in hashmap:
+            if img[i,j] >= 0 and img[i,j] <= rmin:
+                hashmap[img[i,j]] = bot/rmin * img[i,j]
+                #print("bot", hashmap[img[i,j]])
+            elif img[i,j] > rmax and img[i,j] <= 255:
+                hashmap[img[i,j]] = (255 - top) / (255 - rmax) * (img[i,j] - rmax) + top
+                #print("top", hashmap[img[i,j]])
+            elif img[i,j] > rmin and img[i,j] <= rmax:
+                hashmap[img[i,j]] = int(((top - bot) * (img[i,j] - rmin) / (rmax - rmin)) + bot)
+                #print("TF", hashmap[img[i,j]])
+    img2 = img
+    for i,j in it.product(range(img.shape[0]), range(img.shape[1])):
+        #if img2[i,j] in hashmap:
+            #print(img2[i,j], hashmap[img2[i,j]])
+            img2[i,j] = hashmap[img2[i,j]]
+        
+                
+    return img2
+
+
+
+def main():
+    imgpath = sys.argv[1]
+    img = cv2.imread(imgpath)
+    blue = img[:,:,0]
+    green = img[:,:,1]
+    red = img[:,:,2]
+    #plt.figure(figsize=(16,12))
+    #plt.subplot(2,2,3)
+    #color = ('b', 'g', 'r')
+    #for i,col in enumerate(color):
+    #    histr = cv2.calcHist([img],[i],None,[256],[0,256])
+    #    plt.plot(histr,color = col)
+    #    plt.xlim([0,256])
+    
+    bluecs = linearContrastStretchingProcessing(blue)
+    redcs = linearContrastStretchingProcessing(red)
+    greencs = linearContrastStretchingProcessing(green)
+    rgbimg = cv2.merge((bluecs, greencs, redcs))
+    
+    
+    #plt.subplot(2,2,4)
+    #for i,col in enumerate(color):
+    #    histr2 = cv2.calcHist([rgbimg],[i],None,[256],[0,256])
+    #    plt.plot(histr2,color = col)
+    #    plt.xlim([0,256])
+        
+    #plt.subplot(221), plt.imshow(plt.imread(imgpath)), plt.axis('off')
+    #plt.subplot(222), plt.imshow(cv2.cvtColor(rgbimg, cv2.COLOR_RGB2BGR)), plt.axis('off')
+    #plt.savefig('histogram_differences.png')
+    
+    hlsimg = cv2.cvtColor(rgbimg, cv2.COLOR_RGB2HLS)
+    #cv2.imshow("RGB image", rgbimg)
+    #cv2.imshow("HLS image", hlsimg)
+    #cv2.waitKey()
+    hue = hlsimg[:,:,0]
+    light = hlsimg[:,:,1]
+    saturation = hlsimg[:,:,2]
+    #cv2.imshow("hls image", hlsimg)
+    lcs = imf.linearStretch(light)
+    scs = imf.linearStretch(saturation)
+    
+    plt.subplot(121)
+    hist = cv2.calcHist([saturation], [0],None, [256], [0,256])
+    plt.plot(hist)
+    plt.subplot(122)
+    h2 = cv2.calcHist([lcs],[0], None, [256], [0,256])
+    plt.plot(h2)
+    #plt.show()
+    
+    newhls = cv2.merge((hue,lcs,scs))
+    readyRGB = cv2.cvtColor(newhls, cv2.COLOR_HLS2RGB)
+    
+    #cv2.imshow("new hls image", newhls)
+    cv2.imshow("done image", readyRGB)
+    cv2.waitKey()
+
+# o azul como e predominante fazemos contrast stretching ao verde e vermelho para estar a mesma escala pelos limites do azul
+# vemos como fica e avancamos
+
+if(__name__ == "__main__"):
+    main()
