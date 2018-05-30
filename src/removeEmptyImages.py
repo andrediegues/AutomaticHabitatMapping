abf80bcf06a02891845066e2992ee8a2a6ffa2b6
andrediegues <andrediegues@github.com>
2018-05-30 13:49:24 +0100

    Program now produces CSV file with date and different filename according to the log.

--- src/removeEmptyImages.py
+++ src/removeEmptyImages.py
@@ -16,14 +16,17 @@ logpath = os.path.abspath(sys.argv[1])
 os.chdir(logpath)
 new_folder = logpath + '/' + logpath.split('/')[-1]
 value = sys.argv[2]
+new_names = []
+date = logpath.split('/')[-2][-2:] + '/' + logpath.split('/')[-2][-4:-2] + '/' + logpath.split('/')[-2][-6:-4]
+dates = []
 data = pd.read_csv('mra/FilteredPhotos/positions.csv', names=['filename', 'timestamp', 'latitude', 'longitude', 'altitude', 'roll', 'pitch', 'depth', 'entropy'])[1:]
-#print(data['entropy'].astype(float))
-#print(data.entropy.astype(float) <= float(value))
 namesToCopy = data[data['entropy'].astype(float) <= float(value)]
-#print(namesToCopy)
 os.mkdir(new_folder)
 for file in namesToCopy['filename']:
     if os.path.isfile(folder + 'plt_wb_' + file):
-        copyfile(folder + 'plt_wb_' + file, new_folder + '/' + file)
-     
+        copyfile(folder + 'plt_wb_' + file, new_folder + '/' + new_folder.split('/')[-1] + '_' + file)
+        new_names.append(new_folder.split('/')[-1] + '_' + file)
+        dates.append(date)
+namesToCopy.loc[:,('date')] = list(dates)
+namesToCopy.loc[:, ('filename')] = list(new_names)
 namesToCopy.to_csv(new_folder + '/' + logpath.split('/')[-1] + '.csv', index = False)
